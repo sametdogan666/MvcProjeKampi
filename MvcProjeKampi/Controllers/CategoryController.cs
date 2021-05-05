f167cc7dac81c5f8e12ad809cbca851a818f5568
sametdoqann <sametdoqann-s.doqann1570@gmail.com>
2021-05-05 16:31:45 +0300

    5 Mayıs 24. Ders Sonu

--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Business.Concrete;
 
 namespace MvcProjeKampi.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        private CategoryManager categoryManager = new CategoryManager();
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult GetAll()
+        {
+            var categoryValues = categoryManager.GetAll();
+            return View(categoryValues);
+        }
+
     }
 }
