6170bfd3bc38eecb4fbe16d75af3a0b64113effa
sametdoqann <sametdoqann-s.doqann1570@gmail.com>
2021-05-13 08:09:06 +0300

    13 Mayıs 2021 36. Ders Sonu

---
+++ MvcProjeKampi/Controllers/AdminCategoriesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Business.Concrete;
+using Business.ValidationRules.FluentValidation;
+using DataAccess.EntityFramework;
+using Entities.Concrete;
+using FluentValidation.Results;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoriesController : Controller
+    {
+        private CategoryManager _categoryManager = new CategoryManager(new EfCategoryDal());
+        
+        public ActionResult Index()
+        {
+            var categoryValues = _categoryManager.GetAll();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult validationResult = categoryValidator.Validate(category);
+            if (validationResult.IsValid)
+            {
+                _categoryManager.AddCategory(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+    }
+}
