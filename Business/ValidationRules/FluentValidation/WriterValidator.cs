5bec3753a77a3cf93220b68ec5bbf7967de00dc4
sametdoqann <sametdoqann-s.doqann1570@gmail.com>
2021-05-21 02:49:49 +0300

    21 Mayıs 2021 44.Ders Sonu

---
+++ Business/ValidationRules/FluentValidation/WriterValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adını Boş Geçemezsiniz");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen En Az 2 Karakter Girişi Yapınız");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen 50 Karakterden Fazla Değer Girişi Yapmayınız");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar Soyadını Boş Geçemezsiniz");
+            RuleFor(x => x.WriterSurname).MinimumLength(2).WithMessage("Lütfen En Az 2 Karakter Girişi Yapınız");
+            RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("Lütfen 50 Karakterden Fazla Değer Girişi Yapmayınız");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Yazar Hakkında Kısmını Boş Geçemezsiniz");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Yazar Mail Kısmını Boş Geçemezsiniz");
+            RuleFor(p => p.WriterMail).Must(Contains).WithMessage("Email @ işareti içermelidir");
+
+
+        }
+
+        private bool Contains(string arg)
+        {
+            return arg.Contains("@");
+        }
+    }
+}
--- MvcProjeKampi/Controllers/WriterController.cs
+++ MvcProjeKampi/Controllers/WriterController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Business.Concrete;
+using Business.ValidationRules.FluentValidation;
 using DataAccess.EntityFramework;
+using Entities.Concrete;
+using FluentValidation.Results;
 
 namespace MvcProjeKampi.Controllers
 {
@@ -16,6 +19,32 @@ namespace MvcProjeKampi.Controllers
         {
             var WriterValues = _writerManager.GetAll();
             return View(WriterValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddWriter(Writer writer)
+        {
+            WriterValidator writerValidator = new WriterValidator();
+            ValidationResult result = writerValidator.Validate(writer);
+            if (result.IsValid)
+            {
+                _writerManager.AddWriter(writer);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
