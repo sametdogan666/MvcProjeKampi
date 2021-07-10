95126a21bdd8a418d0ef035bf6299c52bb867f1b
sametdoqann <sametdoqann-s.doqann1570@gmail.com>
2021-07-10 19:28:38 +0300

    68.Ders Sonu. Gallery projeye eklendi.

---
+++ MvcProjeKampi/Controllers/GalleryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Business.Concrete;
+using DataAccess.EntityFramework;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+
+        private ImageFileManager _imageFileManager = new ImageFileManager(new EfImageFileDal());
+
+        public ActionResult Index()
+        {
+            var files = _imageFileManager.GetAll();
+            return View(files);
+        }
+    }
+}
