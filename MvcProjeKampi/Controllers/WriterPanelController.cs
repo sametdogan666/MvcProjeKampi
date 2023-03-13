3f1f42bc40e11f26d7275ce1a1cf8cadd46775e4
Samet DOGAN <sametdogan23@protonmail.ch>
2023-03-13 04:24:30 +0300

    87.Ders Sonu. Yazar Profil Sayfası

--- MvcProjeKampi/Controllers/WriterPanelController.cs
+++ MvcProjeKampi/Controllers/WriterPanelController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Business.Concrete;
+using Business.ValidationRules.FluentValidation;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.EntityFramework;
 using Entities.Concrete;
+using FluentValidation.Results;
 using PagedList;
 
 namespace MvcProjeKampi.Controllers
@@ -15,18 +17,44 @@ namespace MvcProjeKampi.Controllers
     {
         private HeadingManager _headingManager = new HeadingManager(new EfHeadingDal());
         private CategoryManager _categoryManager = new CategoryManager(new EfCategoryDal());
+        private WriterManager _writerManager = new WriterManager(new EfWriterDal());
         MvcContext mvcContext = new MvcContext();
 
         // GET: WriterPanel
-        public ActionResult WriterProfile()
+        [HttpGet]
+        public ActionResult WriterProfile(int id = 0)
         {
+            string writerMailInfo = (string)Session["WriterMail"];
+            id = mvcContext.Writers.Where(x => x.WriterMail == writerMailInfo).Select(y => y.WriterId).FirstOrDefault();
+            var writerValue = _writerManager.GetById(id);
+            return View(writerValue);
+        }
+
+        [HttpPost]
+        public ActionResult WriterProfile(Writer writer)
+        {
+            WriterValidator validator = new WriterValidator();
+            ValidationResult result = validator.Validate(writer);
+            if (result.IsValid)
+            {
+                _writerManager.UpdateWriter(writer);
+                return RedirectToAction("AllHeading", "WriterPanel");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
             return View();
         }
 
         public ActionResult MyHeading(string p)
         {
             string writerMailInfo = (string)Session["WriterMail"];
-            var writerIdInfo = mvcContext.Writers.Where(x=>x.WriterMail == writerMailInfo).Select(y=>y.WriterId).FirstOrDefault();
+            var writerIdInfo = mvcContext.Writers.Where(x => x.WriterMail == writerMailInfo).Select(y => y.WriterId).FirstOrDefault();
             var values = _headingManager.GetAllByWriter(writerIdInfo);
 
             return View(values);
@@ -61,11 +89,11 @@ namespace MvcProjeKampi.Controllers
         public ActionResult EditHeading(int id)
         {
             List<SelectListItem> valueCategory = (from x in _categoryManager.GetAll()
-                select new SelectListItem
-                {
-                    Text = x.CategoryName,
-                    Value = x.CategoryId.ToString()
-                }).ToList();
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryId.ToString()
+                                                  }).ToList();
             ViewBag.vlc = valueCategory;
             var HeadingValue = _headingManager.GetById(id);
             return View(HeadingValue);
@@ -88,7 +116,7 @@ namespace MvcProjeKampi.Controllers
 
         public ActionResult AllHeading(int page = 1)
         {
-            var headings = _headingManager.GetAll().ToPagedList(page,3);
+            var headings = _headingManager.GetAll().ToPagedList(page, 3);
             return View(headings);
         }
 
